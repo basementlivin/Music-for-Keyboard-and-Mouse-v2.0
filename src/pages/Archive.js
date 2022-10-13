2ab1879f9e4420eebbc80cccf8ec6432b4eb2182
basementlivin <epedersen13@gmail.com>
2022-10-13 13:16:16 -0400

    Successfully pulling DB data to frontend

--- src/pages/Archive.js
+++ src/pages/Archive.js
@@ -4,7 +4,6 @@ import { useEffect, useState } from 'react';
 
 export default function Archive () {
     const BASE_URL = process.env.REACT_APP_BASE_URL;
-    console.log(BASE_URL);
     const [recordings, setRecordings ] = useState([]);
     const getRecordings = async () => {
         try {
@@ -17,12 +16,23 @@ export default function Archive () {
     }
 
     useEffect(()=>{getRecordings()}, [])
-
-    console.log(`Hey chum, looks like there are $%{recordings.length} available to render`)
-
+    // console.log(recordings)
+    // console.log(`Hey chum, looks like there are ${recordings.length} recordings available to render`)
+    
     return (
         <div>
             <h1>Music for Keyboard and Mouse, The Collected Recordings (2022—present)</h1>
+            <div className="archive-wrapper">
+                <div className="recording-divider"></div>
+                    <div className="recording-wrapper">
+                        <ul>
+                            {recordings.map(recording => <li>{recording.artistname}</li>)}
+                            {recordings.map(recording => <li>{recording.location}</li>)}
+                            {recordings.map(recording => <li>{recording.environment}</li>)}
+                        </ul>
+                    </div>
+                <div className="recording-divider"></div>
+            </div>
         </div>
     )
 }
